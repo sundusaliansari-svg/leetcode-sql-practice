e87b455afac4f2caed3f3116ebe30046dabd7bc4
sundusaliansari-svg <sundus.ansari.official@gmail.com>
2025-09-25 07:46:44 +0530

    Create 01_combine_two_tables.sql

---
+++ stage-1-core-foundations/01_combine_two_tables.sql
@@ -0,0 +1,20 @@
+/* ============================================================
+   PROBLEM: Combine Two Tables
+   SOURCE: https://leetcode.com/problems/combine-two-tables/
+   TOPIC: Joins & Aggregations
+   ============================================================ */
+
+/* Problem Statement:
+Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+Return the result table in any order.
+*/
+
+-- Solution
+Select 
+ p.firstName,
+ p.lastName,
+ a.city,
+ a.state
+From person p
+Left Join address a
+On p.personId = a.personId;
